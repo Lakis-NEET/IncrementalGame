e7d83701bec900b405967caa38cba306272be86a
Lakis-NEET <nasikiqbal@gmail.com>
2021-09-11 18:08:33 +0700

    first commit

---
+++ Assets/Script/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource soundKoin;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        soundKoin.PlayOneShot(soundKoin.clip);
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
